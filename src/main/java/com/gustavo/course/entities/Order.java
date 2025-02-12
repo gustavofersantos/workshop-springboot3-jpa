cccf52b91281d3b934ef998262ccab09a3b20230
Dannes <gustavofdev@gmail.com>
2025-02-12 17:03:25 -0300

    feature: Category and Product resources, services and repositories

--- src/main/java/com/gustavo/course/entities/Order.java
+++ src/main/java/com/gustavo/course/entities/Order.java
@@ -8,8 +8,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gustavo.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
--- src/main/java/com/gustavo/course/resources/CategoryResource.java
+++ src/main/java/com/gustavo/course/resources/CategoryResource.java
@@ -9,26 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gustavo.course.entities.Order;
-import com.gustavo.course.services.OrderService;
+import com.gustavo.course.entities.Category;
+import com.gustavo.course.services.CategoryService;
 
 @RestController
-@RequestMapping(value="/orders")
-public class OrderResource {
+@RequestMapping(value="/categories")
+public class CategoryResource {
 	
 	@Autowired
-	private OrderService service;
+	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Order>> findAll() {
-		List <Order> list = service.findAll();
+	public ResponseEntity<List<Category>> findAll() {
+		List <Category> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<Order> findById(@PathVariable Long id){
-		Order obj = service.findById(id);
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
